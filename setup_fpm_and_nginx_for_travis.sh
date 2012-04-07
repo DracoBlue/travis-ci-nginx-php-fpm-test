2a2a1f3ec8ca4fad21fb07dc35d2e142762107c8
DracoBlue <JanS@DracoBlue.de>
2012-04-07 21:52:31 +0200

    output current working directory

--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -5,6 +5,7 @@ apt-get install php5-fpm
 cat /etc/nginx/nginx.conf
 ls /etc/nginx
 
+pwd
 cp .travis_nginx.conf /etc/nginx/nginx.conf
 sed -i '' "s/%%HOME_DIRECTORY/`pwd`/" /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf

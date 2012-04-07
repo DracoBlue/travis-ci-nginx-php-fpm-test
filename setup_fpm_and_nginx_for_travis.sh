c7e76c072a76f7f1c39670f1e12bcab1f90257cb
DracoBlue <JanS@DracoBlue.de>
2012-04-07 21:40:26 +0200

    moved all into a small script

--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt-get install nginx php5-fpm
+apt-get install nginx php5-fpm
 
 cat /etc/nginx/nginx.conf
 ls /etc/nginx
 
-sudo cp .travis_nginx.conf /etc/nginx/nginx.conf
-sudo sed -i '' "s/%%HOME_DIRECTORY/`pwd`/" /etc/nginx/nginx.conf
+cp .travis_nginx.conf /etc/nginx/nginx.conf
+sed -i '' "s/%%HOME_DIRECTORY/`pwd`/" /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
-sudo /etc/init.d/nginx restart
+/etc/init.d/nginx restart
